f901c24e1bbc426c44139b62ab61974d1afccb45
Brenna Switzer <brenna.switzer@gmail.com>
2015-12-09 22:43:04 -0800

    Changing name of main script to day seven

--- test/spec/test.js
+++ test/spec/test.js
@@ -12,4 +12,13 @@
     });
   });
 
+  describe('Array', function() {
+    describe('#getType()', function () {
+      it('should return -1 when the value is not present', function () {
+        assert.equal(-1, [1,2,3].indexOf(5));
+        assert.equal(-1, [1,2,3].indexOf(0));
+      });
+    });
+  });
+
 })();
