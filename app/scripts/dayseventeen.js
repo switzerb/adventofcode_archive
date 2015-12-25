25782bc8fac58af6e97e1fef346cb57e9e4155b7
Brenna Switzer <brenna.switzer@gmail.com>
2015-12-25 14:29:09 -0800

    Initial commit of day seventeen

--- app/scripts/dayseventeen.js
+++ app/scripts/dayseventeen.js
@@ -1,10 +1,16 @@
 
+//return the count
+// counting the dept of recursion using parameter to pass through
+
+
 var containers = [33, 14, 18, 20, 45, 35, 16, 35, 1, 13, 18, 13, 50, 44, 48, 6, 24, 41, 30, 42];
 var testContainers = [20, 15, 10, 5, 5];
-var combos = [];
 
-function fillContainers(available,remaining) {
-  debugger
+function fillContainers(available,remaining, depth) {
+  //debugger
+  if (! depth) {
+    depth = 0;
+  }
   var count = 0;
 
   for (var i = 0; i < available.length; i++) {
@@ -12,17 +18,17 @@ function fillContainers(available,remaining) {
     var container = available[i];
 
       if (container == remaining) {
+        console.log("got one", depth);
         count++;
       } else if (container > remaining) {
           //I don't think we need to do anything else. uncertain
       } else if (container < remaining) {
 
-        count += fillContainers(available.slice(i+1),remaining - container);
+        count += fillContainers(available.slice(i+1),remaining - container,depth+1);
 
       }
 
   }
-
   return count;
 }
 
@@ -43,7 +49,5 @@ function getCount(containers,target){
   return combinations;
 }
 
-console.log(getCount(testContainers,25));
+//console.log(getCount(testContainers,25));
 console.log(getCount(containers,150));
-
-console.log(combos);
