9ac1c967678962b602c0b9ffdaa43242875b3a62
brennaswitzer <brenna.switzer@emergeinteractive.com>
2017-12-02 20:49:36 -0800

    Day Two, Part One (with correct answer)

--- aoc2017/daytwo/puzzle.py
+++ aoc2017/daytwo/puzzle.py
@@ -1,9 +1,35 @@
+# --- Day 2: Corruption Checksum ---
+# http://adventofcode.com/2017/day/2 #
+
+import functools
+
+
+# read in unique puzzle text
+# opens a file and reads into a return value
+def get_input():
+    fh = open("input.txt")
+    file_input = fh.read().strip().splitlines()
+    fh.close()
+    return file_input
+
+
+def parse(data):
+    return list(map(lambda x: list(map(lambda y: int(y), x.split(','))), data))
+
+
+def get_max(data):
+    return max(data)
+
+
+def get_min(data):
+    return min(data)
 
 
 # answer to puzzle part one
 # puzzle -> answer
-def part_one():
-    pass
+def part_one(puzzle_input):
+    calc = map(lambda x: get_max(x) - get_min(x), puzzle_input)
+    return functools.reduce(lambda a, i: a + i, calc, 0)
 
 
 # answer to puzzle part two
@@ -21,4 +47,4 @@ def get_encouragement():
     return puzzle_input
 
 
-print(get_encouragement())
+print(part_one(parse(get_input())))
--- aoc2017/daytwo/test_daytwo.py
+++ aoc2017/daytwo/test_daytwo.py
@@ -4,5 +4,14 @@ import aoc2017.daytwo.puzzle as puzzle
 
 class TestDayTwo(unittest.TestCase):
 
-    def test_get_encouragement(self):
-        self.assertEqual(puzzle.get_encouragement(), "You can do this.")
+    def test_example_one(self):
+        self.assertEqual(puzzle.part_one([[5, 1, 9, 5], [7, 5, 3],[2, 4, 6, 8]]), 18)
+
+    def test_get_max(self):
+        self.assertEqual(puzzle.get_max([5, 1, 9, 5]), 9)
+
+    def test_get_min(self):
+        self.assertEqual(puzzle.get_min([5, 1, 9, 5]), 1)
+
+    def test_part_one(self):
+        self.assertEqual(puzzle.part_one(puzzle.parse(puzzle.get_input())), 42299)
