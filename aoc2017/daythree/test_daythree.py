7cbd99fc5d47ddca71502f01db59aa3bfd425f27
brennaswitzer <brenna.switzer@emergeinteractive.com>
2017-12-03 18:58:02 -0800

    Add unit tests

--- aoc2017/daythree/test_daythree.py
+++ aoc2017/daythree/test_daythree.py
@@ -4,5 +4,16 @@ import aoc2017.daythree.puzzle as puzzle
 
 class TestDayThree(unittest.TestCase):
 
-    def test_get_encouragement(self):
-        self.assertEqual(puzzle.get_encouragement(), "You can do this.")
+    def test_example_one(self):
+        self.assertEqual(puzzle.part_one('1'), 0)
+
+    def test_example_two(self):
+        self.assertEqual(puzzle.part_one('12'), 3)
+
+    def test_example_three(self):
+        self.assertEqual(puzzle.part_one('23'), 2)
+
+    def test_example_four(self):
+        self.assertEqual(puzzle.part_one('1024'), 31)
+
+
