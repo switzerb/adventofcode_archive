35bbcfb1898844ccb0892736ce7c7f000c99521e
brennaswitzer <brenna.switzer@emergeinteractive.com>
2017-12-09 22:13:06 -0800

    Day five. Ten minutes and ten lines of code, goddamn it. Not functional.

--- aoc2017/dayfive/puzzle.py
+++ aoc2017/dayfive/puzzle.py
@@ -20,34 +20,22 @@ def parse(p_input):
     return list(map(lambda x: int(x), p_input.splitlines()))
 
 
-def execute(n, i):
-    instructions = n[i]['instructions'][:]
-    current = n[i]['current']
-    temp = current
-
-    try:
-        current += instructions[current]
-        instructions[temp] += 1
-        return n + [{'instructions': instructions, 'current': current}]
-    except IndexError:
-        return n + n
-
-
-def is_exit(state):
-    if state[1]['current'] < 0:
-        return False
-    if state[1]['current'] >= len(state[1]['instructions']):
-        return False
-    return True
-
-
 # answer to puzzle part one: get in a list of instructions and reduce until we "escape" the list -- meaning we are out of range on our list
 # the answer is the index value, which is the number of steps we've taken to get out of range
 # list -> index
 def part_one(p_input):
-    init = [{'instructions': p_input, 'current': 0}]
-    answer = itertools.dropwhile(is_exit, zip(itertools.count(start=0, step=1), functools.reduce(execute, itertools.count(start=0, step=1), init)))
-    return list(itertools.islice(answer, 1))
+    answer = 0
+    current = 0
+
+    for i in range(0, 400000):
+        try:
+            temp = current
+            current += p_input[current]
+            p_input[temp] += 1
+        except IndexError:
+            answer = i
+            break
+    return answer
 
 
 # answer to puzzle part two
