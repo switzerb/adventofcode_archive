e41584bba63bc623729c529123849738ec05ddbd
brennaswitzer <brenna.switzer@emergeinteractive.com>
2017-12-09 21:47:37 -0800

    Day five. No answer -- not performant enough so can't get an answer in time

--- aoc2017/dayfive/puzzle.py
+++ aoc2017/dayfive/puzzle.py
@@ -1,3 +1,10 @@
+# --- Day 5: A Maze of Twisty Trampolines, All Alike ---
+# http://adventofcode.com/2017/day/5 #
+
+import functools
+import itertools
+
+example = [0, 3, 0, 1, -3]
 
 
 # read in unique puzzle text
@@ -9,10 +16,38 @@ def get_input():
     return file_input
 
 
-# answer to puzzle part one
-# puzzle -> answer
-def part_one():
-    pass
+def parse(p_input):
+    return list(map(lambda x: int(x), p_input.splitlines()))
+
+
+def execute(n, i):
+    instructions = n[i]['instructions'][:]
+    current = n[i]['current']
+    temp = current
+
+    try:
+        current += instructions[current]
+        instructions[temp] += 1
+        return n + [{'instructions': instructions, 'current': current}]
+    except IndexError:
+        return n + n
+
+
+def is_exit(state):
+    if state[1]['current'] < 0:
+        return False
+    if state[1]['current'] >= len(state[1]['instructions']):
+        return False
+    return True
+
+
+# answer to puzzle part one: get in a list of instructions and reduce until we "escape" the list -- meaning we are out of range on our list
+# the answer is the index value, which is the number of steps we've taken to get out of range
+# list -> index
+def part_one(p_input):
+    init = [{'instructions': p_input, 'current': 0}]
+    answer = itertools.dropwhile(is_exit, zip(itertools.count(start=0, step=1), functools.reduce(execute, itertools.count(start=0, step=1), init)))
+    return list(itertools.islice(answer, 1))
 
 
 # answer to puzzle part two
@@ -21,13 +56,4 @@ def part_two():
     pass
 
 
-# my version of hello world: it takes nihilism and despair and returns encouragement
-# none -> string
-def get_encouragement():
-    fh = open("input.txt")
-    puzzle_input = fh.read().strip()
-    fh.close()
-    return puzzle_input
-
-
-print(get_encouragement())
+print(part_one(parse(get_input())))
--- aoc2017/dayfive/test_dayfive.py
+++ aoc2017/dayfive/test_dayfive.py
@@ -4,5 +4,5 @@ import aoc2017.dayfive.puzzle as puzzle
 
 class TestDayFive(unittest.TestCase):
 
-    def test_get_encouragement(self):
-        self.assertEqual(puzzle.get_encouragement(), "You can do this.")
+    def test_example_one(self):
+        self.assertEqual(puzzle.part_one([0, 3, 0, 1, -3]), 5)
