4decb164e8474f33ed481897f0c80fee9deee3f7
Brenna Switzer <brenna.switzer@gmail.com>
2019-12-09 17:21:57 -0800

    Adding debugging tools

--- 2019/lib/computer.rb
+++ 2019/lib/computer.rb
@@ -1,9 +1,12 @@
 ## --- Day 2: 1202 Program Alarm ---  ##
 # https://adventofcode.com/2019/day/2  #
+require 'pry'
+
  class Computer
 
-  def initialize
+  def initialize(input)
 		@diagnostic = 0
+		@in = input
   end
 
   def load(program)
@@ -37,6 +40,7 @@
   def run
     @ip = 0
     until @ip >= @program.length do
+			# binding.pry
 			instructions = @program[@ip]
       code = get_code(instructions)
 
@@ -44,16 +48,14 @@
       p1.mode = get_mode(instructions, 1)
       p1.value = @program[@ip + 1]
       
-      if(code == 2 || code == 1) then
-        p2 = Instruction.new
-        p2.mode = get_mode(instructions, 2)
-        p2.value = @program[@ip + 2]
+      p2 = Instruction.new
+      p2.mode = get_mode(instructions, 2)
+      p2.value = @program[@ip + 2]
 
-        address = @program[@ip + 3]
-      end
+      address = @program[@ip + 3]
 
      return if code == 99
-     raise "" unless (code == 1 || code == 2 || code == 3 || code == 4 || code == 99)
+		 raise "" unless (code.between?(1,8) || code == 99)
 
      if code == 1
        opcode_1(p1, p2, address)
@@ -62,11 +64,21 @@
        opcode_2(p1, p2, address)
        @ip += 4
      elsif code == 3
-      opcode_3(1, p1)
+      opcode_3(@in, p1)
       @ip += 2
      elsif code == 4
       opcode_4(p1)
       @ip += 2
+		 elsif code == 5
+			 opcode_5(p1,p2)
+		 elsif code == 6
+			 opcode_6(p1,p2)
+		 elsif code == 7
+			 opcode_7(p1, p2, address)
+			 @ip += 4
+		 elsif code == 8
+			 opcode_8(p1, p2, address)
+			 @ip += 4
      end
 			# puts "count #{@ip}"
     end
@@ -94,6 +106,42 @@
 		# puts "output: #{@program[p1.value]}"
 		@diagnostic = @program[p1.value]
   end
+
+	def opcode_5(p1,p2)
+		v1 = p1.mode == 0 ? @program[p1.value] : p1.value
+		v2 = p2.mode == 0 ? @program[p2.value] : p2.value
+
+		if v1 != 0
+			@ip = v2
+		else
+			@ip += 3
+		end
+	end
+  
+  def opcode_6(p1, p2)
+		v1 = p1.mode == 0 ? @program[p1.value] : p1.value
+		v2 = p2.mode == 0 ? @program[p2.value] : p2.value
+
+		if v1 == 0
+			@ip = v2
+		else
+			@ip += 3
+		end
+	end
+
+	def opcode_7(p1,p2,address)
+		v1 = p1.mode == 0 ? @program[p1.value] : p1.value
+		v2 = p2.mode == 0 ? @program[p2.value] : p2.value
+
+		v1 < v2 ? @program[address] = 1 : @program[address] = 0
+	end
+
+	def opcode_8(p1,p2,address)
+		v1 = p1.mode == 0 ? @program[p1.value] : p1.value
+		v2 = p2.mode == 0 ? @program[p2.value] : p2.value
+
+		v1 == v2 ? @program[address] = 1 : @program[address] = 0
+	end
 
   def finish
     @program
