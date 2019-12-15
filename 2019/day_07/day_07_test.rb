22072e02a07290955a4373a676ca583d9d26cf90
Brenna Switzer <brenna.switzer@gmail.com>
2019-12-15 12:27:30 -0800

    day 07 part two complete

--- 2019/day_07/day_07_test.rb
+++ 2019/day_07/day_07_test.rb
@@ -13,9 +13,6 @@
 
  class Day07Test < Minitest::Test
    def setup
-		phase_array = [0,1,2,3,4]
-		@phase_sequences = phase_array.permutation.to_a
-
 		file = File.open(__dir__ + "/input.txt")
 		program = file.read
 		program = program.split(",")
@@ -24,25 +21,37 @@
 
    def test_day7_part_one
 			max = 0
+			sequences = [0,1,2,3,4].permutation.to_a
 
-			@phase_sequences.each do |seq|
-				signal = 0
-				seq.each do |p|
-					amp = Computer.new
-					amp.set_in(p)
-					amp.set_in(signal)
-					amp.load(@program)
-					amp.run
-					signal = amp.get_out
-				end
+			sequences.each do |seq|
+				amp = Amplifier.new seq
+				amp.load(@program)
+				amp.run
+				signal = amp.to_thrusters
 
 				if signal > max
 					max = signal
 				end
 			end
-
      assert_equal 844468, max
    end
+
+	 def test_day7_part_two
+			max = 0
+			sequences = [5,6,7,8,9].permutation.to_a
+
+			sequences.each do |seq|
+				amp = Amplifier.new seq
+				amp.load(@program)
+				amp.run
+				signal = amp.to_thrusters
+
+				if signal > max
+					max = signal
+				end
+			end
+			assert_equal 4215746, max
+	 end
 
 	 def test_day7_amp_1
 		phases = [9,8,7,6,5]
@@ -50,5 +59,13 @@
 		amp.load([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5])
 		amp.run
 		assert_equal 139629729, amp.to_thrusters
+	 end
+
+	 def test_day7_amp_2
+		phases = [9,7,8,5,6]
+		amp = Amplifier.new phases
+		amp.load([3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10])
+		amp.run
+		assert_equal 18216, amp.to_thrusters
 	 end
  end
--- 2019/day_07/solver.rb
+++ 2019/day_07/solver.rb
@@ -3,8 +3,10 @@ require 'fiber'
 require 'pry'
 require_relative 'amplifier'
 
-phase_array = [0,1,2,3,4]
-phase_sequences = phase_array.permutation.to_a
+amp_array = [0,1,2,3,4]
+feedback_array = [5,6,7,8,9]
+for_amps = amp_array.permutation.to_a
+for_feedback = [5,6,7,8,9].permutation.to_a
 
 file = File.open(__dir__ + "/input.txt")
 program = file.read
@@ -14,82 +16,31 @@ program = program.map(&:to_i)
 max = 0
 $debug = false
 
-phases = [9,8,7,6,5]
+for_feedback.each do |seq|
+	amp = Amplifier.new seq
+	amp.load(program)
+	amp.run
+	boost = amp.to_thrusters
 
-tester = Amplifier.new phases
-tester.load([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5])
-tester.run
-puts tester.to_thrusters
-
-exit
-
-amps = Array.new
-fibers = Array.new
-phases = [9,8,7,6,5]
-A,B,C,D,E = 0,1,2,3,4
-
-# create, load, provide phases and start each of the five amplifiers in a fiber
-5.times do |i|
-	amps[i] = Computer.new
-	amps[i].load([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5])
-	amps[i].set_in(phases[i])
-	fibers[i] = Fiber.new { amps[i].run }
-end
-
-amps[A].set_in(0)
-
-def some_alive?(fibers)
-	fibers.each do |f|
-		return true if f.alive?
+	if boost > max
+		max = boost
 	end
-	false
 end
 
-phases.each do |phase|
-  while some_alive?(fibers)
-		(0...5).each do |i|
-			binding.pry if $debug
-			prev = (i - 1) % amps.length
-			signal = amps[prev].get_out
-			amps[i].set_in(signal) unless signal.nil?
-			fibers[i].resume
-	  end
-	end
-	puts amps[E].get_out
-end
+puts "part two: " + max.to_s
 
+max = 0
 
-exit
-
-phase_sequences.each do |seq|
-	signal = 0
-	seq.each do |p|
-		amp = Computer.new
+for_amps.each do |seq|
+		amp = Amplifier.new seq
 		amp.load(program)
-		fiber = Fiber.new { amp.run }
-		fiber.resume
-		amp.set_in(p)
-		amp.set_in(signal)
-		fiber.resume
-		signal =  amp.get_out
-		binding.pry if $debug
-	end
+		amp.run
+		signal = amp.to_thrusters
 
 	if signal > max
 		max = signal
 	end
 end
 
-puts "part one:" + max.to_s
-
-
-
-
-#f = Fiber.new { amps["A"].run }
-#f.resume
-#amps["A"].set_in(3)
-#f.resume
-#amps["A"].set_in(0)
-#f.resume
-#puts amps["A"].get_out
+puts "part one: " + max.to_s
 
