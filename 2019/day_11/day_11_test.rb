e057993b1f3b763dbfaa72bb730093a50a7edd3f
Brenna Switzer <brenna.switzer@gmail.com>
2019-12-21 21:00:00 -0800

    Day 11 initial commit

---
+++ 2019/day_11/day_11_test.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+  begin
+    gem 'minitest', '>=5.0.0'
+    require 'minitest/autorun'
+    require_relative 'day_11.rb'
+  rescue Gem::LoadError => e
+    puts "\nError:\n#{e.backtrace.first} #{e.message}"
+    puts DATA.read
+    exit 1
+  end
+
+ class SolverTest < Minitest::Test
+   def setup
+     @day = Solver.new
+   end
+
+   def test_hello
+     assert_equal "hello, world!", @day.hello
+   end
+ end
